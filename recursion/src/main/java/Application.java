faaf355e3000b48d4eec1df66437783ec9e34941
psadovski <pawelsadowski92@gmail.com>
2018-09-25 11:51:52 +0200

    4.Rekurencja | dodane zadanie 9 rozwiązane bez rekurencji

--- recursion/src/main/java/Application.java
+++ recursion/src/main/java/Application.java
@@ -1,15 +1,15 @@
 public class Application {
     public static void main(String[] args) {
 
-        int valuea = 6;
+        int value = 6;
 
-        for (int i = 1; i <= valuea; i++) {
+        for (int i = 1; i <= value; i++) {
             System.out.println(Task1.countSequence(i));
         }
 
         System.out.println();
 
-        for (int i = 1; i <= valuea; i++) {
+        for (int i = 1; i <= value; i++) {
             System.out.println(Task2.f(i));
         }
 
@@ -17,5 +17,7 @@ public class Application {
         System.out.println(Task3.goldenRatio(25));
         System.out.println();
         System.out.println(Task4.mystery(4, 18));
+        System.out.println();
+        System.out.println(Task5.f(value));
     }
 }
