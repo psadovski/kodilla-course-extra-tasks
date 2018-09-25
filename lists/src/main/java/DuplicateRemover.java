dc3dae0b0b3c2b7057f3dad186b3a544b3a85334
psadovski <pawelsadowski92@gmail.com>
2018-09-25 12:49:08 +0200

    3.Laczenie dwoch list i usuwanie duplikatów | po zmianach

--- lists/src/main/java/DuplicateRemover.java
+++ lists/src/main/java/DuplicateRemover.java
@@ -1,14 +1,10 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class DuplicateRemover {
     public List<Integer> removeDuplicates(List<Integer> list) {
-        for (int n = 0; n < list.size(); n++) {
-            for (int i = 0; i < list.size(); i++) {
-                if (n != i && list.get(n) == list.get(i)) {
-                    list.remove(i);
-                }
-            }
-        }
-        return list;
+        List<Integer> listWithoutDuplicates = new ArrayList<>(new LinkedHashSet<>(list));
+        return listWithoutDuplicates;
     }
 }
--- lists/src/main/java/ListMerge.java
+++ lists/src/main/java/ListMerge.java
@@ -1,16 +1,12 @@
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.TreeSet;
 
 public class ListMerge {
     public List<Integer> merge(List<Integer> list1, List<Integer> list2) {
         list1.addAll(list2);
-        list1.sort(Comparator.naturalOrder());
-
-        for (int n = 1; n < list1.size(); n++) {
-            if (list1.get(n) == list1.get(n - 1)) {
-                list1.remove(list1.get(n));
-            }
-        }
-        return list1;
+        List<Integer> newList = new ArrayList<>(new TreeSet<>(list1));
+        return newList;
     }
 }
